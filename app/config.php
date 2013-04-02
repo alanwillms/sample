0f9c7a3d2c624abe6836b793c03231036459a6fb
Alan Willms <dyulax@gmail.com>
2013-04-02 12:04:07 -0300

    Change config  to use getenv()

--- app/config.php
+++ app/config.php
@@ -1,14 +1,14 @@
 <?php
 return array(
 	'url' => array(
-		'defaultController' => 'index',
+		'defaultController' => 'names',
 		'defaultAction' => 'index',
 	),
 	'dataBase' => array(
-		'engine'   => $_ENV['db_engine'],
-		'host'     => $_ENV['db_host'],
-		'database' => $_ENV['db_name'],
-		'username' => $_ENV['db_user'],
-		'password' => $_ENV['db_password'],
+		'engine'   => getenv('DB_ENGINE'),
+		'host'     => getenv('DB_HOST'),
+		'database' => getenv('DB_NAME'),
+		'username' => getenv('DB_USER'),
+		'password' => getenv('DB_PASSWORD'),
 	),
 );
