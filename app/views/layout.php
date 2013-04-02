c89a7ad178b0638707ed298a3cbd0edb2cc3505d
Alan Willms <dyulax@gmail.com>
2013-04-02 12:42:34 -0300

    Add empty layout

---
+++ app/views/layout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Code Sample</title>
+		<link rel="stylesheet" href="<?php echo $this->getBaseUrl(); ?>/assets/css/bootstrap.min.css" />
+	</head>
+	<body>
+		<?php echo $content; ?>
+	</body>
+</html>
