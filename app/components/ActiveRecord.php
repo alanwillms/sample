316db5be541245c588cce225ea15769c626aa1a1
Alan Willms <dyulax@gmail.com>
2013-04-02 13:47:09 -0300

    ActiveRecord checking table properties

--- app/components/ActiveRecord.php
+++ app/components/ActiveRecord.php
@@ -12,6 +12,12 @@ abstract class ActiveRecord
 	private static $_db;
 
 	/**
+	 * Information about database tables
+	 * @var array
+	 */
+	private static $_dbTableInfo = array();
+
+	/**
 	 * Set PDO DB adapter
 	 * @param PDO $dbAdapter
 	 */
@@ -39,6 +45,53 @@ abstract class ActiveRecord
 	}
 
 	/**
+	 * Return informations about database table for current model.
+	 * @return array
+	 */
+	public static function getDbTableInfo()
+	{
+		$model = get_called_class();
+
+		if (!isset(self::$_dbTableInfo[$model])) {
+
+			$statement = $model::getDb()->prepare('DESCRIBE ' . $model::getDbTableName());
+
+			$statement->execute();
+
+			self::$_dbTableInfo[$model] = $statement->fetchAll(PDO::FETCH_ASSOC);
+		}
+
+		return self::$_dbTableInfo[$model];
+	}
+
+	/**
+	 * Return attributes names
+	 * @return array
+	 */
+	public static function getAttributes()
+	{
+		$model = get_called_class();
+		$attributes = array();
+
+		foreach ($model::getDbTableInfo() as $column) {
+			$attributes[$column['Field']] = $column['Field'];
+		}
+
+		return $attributes;
+	}
+
+	/**
+	 * Check if model contains certain attribute
+	 * @param string $attribute
+	 * @return boolean
+	 */
+	public static function hasAttribute($attribute)
+	{
+		$model = get_called_class();
+		return in_array($attribute, $model::getAttributes());
+	}
+
+	/**
 	 * Find All objects
 	 * @param array $filtering Specifies query params (as WHERE, ORDER, etc.)
 	 * @return ActiveRecord[]
@@ -50,7 +103,7 @@ abstract class ActiveRecord
 		$select = $where = $order = $limit = null;
 
 		if (isset($filtering['order'])) {
-			$order = ' ORDER BY ' . $model::prepareOrder($filtering['order']);
+			$order = $model::prepareOrder($filtering['order']);
 		}
 
 		if (!isset($filtering['select'])) {
@@ -62,18 +115,13 @@ abstract class ActiveRecord
 		$statement = $model::getDb()->query($query);
 
 		if (!$statement) {
+			die($query);
 			throw new Exception('Database query error');
 		}
 
 		$statement->setFetchMode(PDO::FETCH_CLASS, $model);
 
-		$models = array();
-
-		while ($model = $statement->fetch()) {
-			$models[] = $model;
-		}
-
-		return $models;
+		return $statement->fetchAll();
 	}
 
 	/**
@@ -93,6 +141,10 @@ abstract class ActiveRecord
 			}
 		}
 
-		return implode(', ', $orderBy);
+		if ($orderBy) {
+			return ' ORDER BY ' . implode(', ', $orderBy);
+		}
+
+		return null;
 	}
 }
